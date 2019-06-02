0b6d9d560c8269e1d434bcd6ccb04562421de1f1
Prabhat Sharma <hi.prabhat@gmail.com>
2019-06-02 04:30:28 -0700

    testing release

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // HandlerFunc1 handles basic calls
 func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\nRelease 1")
+	fmt.Fprintf(w, "\nRelease 1.01")
 }
 
 func main() {
